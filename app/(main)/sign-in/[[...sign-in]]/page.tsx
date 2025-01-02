3aa87e7fc9f31984e9cb6dc2a113ee666d15c001
zacharia muigai <zacheyshop@gmail.com>
2025-01-02 15:05:35 +0300

    auth loggin

--- app/(main)/sign-in/[[...sign-in]]/page.tsx
+++ app/(main)/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,7 @@
 import { SignIn } from "@clerk/nextjs";
 import React from "react";
 
-export default async function Page({ searchParams }: any) {
+export default async function Page() {
   return (
     <div className=" h-[calc(100vh-60px)] bg-black overflow-hidden w-full">
       <div className="flex justify-center items-center h-full w-full shadow-slate-700 shadow-xl">
--- components/UsersButton.tsx
+++ components/UsersButton.tsx
@@ -25,7 +25,6 @@ const UsersButton: React.FC<UsersButtonProps> = ({
   mylink,
   myloading,
   mydisabled,
-  mygigip,
 }) => {
   return (
     <>
