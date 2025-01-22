dbff18b3be184e672744fd42950f2c54fea1d582
zacheys123 <105823151+zacheys123@users.noreply.github.com>
2025-01-22 23:23:32 +0300

    Update FriendsComponent.tsx

--- components/friend/FriendsComponent.tsx
+++ components/friend/FriendsComponent.tsx
@@ -50,7 +50,7 @@ const FriendsComponent = () => {
             username: "",
             followers: [],
             followings: [],
-            videos: [],
+            
             allreviews: [],
             myreviews: [],
           });
