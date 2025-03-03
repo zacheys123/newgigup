138f9b4d1f442067f57f0a45b6d08d4874ec0379
zacharia muigai <zacheyshop@gmail.com>
2025-03-03 16:08:30 +0300

    new info added

--- config.ts
+++
@@ -1,8 +0,0 @@
-const config = {
-  production: "https://gigupserver-d94fc174e3bf.herokuapp.com",
-  development: "http://localhost:8080",
-};
-
-export const SOCKET_URL = () => {
-  return config[process.env.NODE_ENV || "development"];
-};
