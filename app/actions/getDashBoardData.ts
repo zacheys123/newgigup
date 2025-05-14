4276d67e5bfd3aa899928b202083af45602bf94c
zacharia muigai <zacheyshop@gmail.com>
2025-05-14 18:14:26 +0300

    added new fucntionality for free and pro users

--- app/actions/getDashBoardData.ts
+++ app/actions/getDashBoardData.ts
@@ -1,61 +1,63 @@
-import connectDb from "@/lib/connectDb";
-import User from "@/models/user";
-import { DashboardData } from "@/types/dashboard";
+// import connectDb from "@/lib/connectDb";
+// import User from "@/models/user";
+// import { DashboardData } from "@/types/dashboard";
 
-export async function getDashboardData(userId: string): Promise<DashboardData> {
-  try {
-    console.log("Connecting to DB...");
-    await connectDb();
-    console.log("DB connected");
+// export async function getDashboardData(userId: string): Promise<DashboardData> {
+//   try {
+//     console.log("Connecting to DB...");
+//     await connectDb();
+//     console.log("DB connected");
 
-    if (!userId) {
-      throw new Error("Unauthorized");
-    }
+//     if (!userId) {
+//       throw new Error("Unauthorized");
+//     }
 
-    console.log("Looking for user with clerkId:", userId);
-    const user = await User.findOne({ clerkId: userId }).select([
-      "isClient",
-      "isMusician",
-      "firstLogin",
-      "clerkId",
-      "earnings",
-      "monthlyGigsBooked",
-      "monthlyGigsPosted",
-      "totalSpent",
-      "tier",
-      "nextBillingDate",
-      "onboardingComplete",
-    ]);
+//     console.log("Looking for user with clerkId:", userId);
+//     const user = await User.findOne({ clerkId: userId }).select([
+//       "isClient",
+//       "isMusician",
+//       "firstLogin",
+//       "clerkId",
+//       "earnings",
+//       "monthlyGigsBooked",
+//       "monthlyGigsPosted",
+//       "totalSpent",
+//       "tier",
+//       "nextBillingDate",
+//       "onboardingComplete",
+//     ]);
 
-    console.log("User found:", user);
+//     console.log("User found:", user);
 
-    if (!user) {
-      throw new Error("User not found");
-    }
+//     if (!user) {
+//       throw new Error("User not found");
+//     }
 
-    return {
-      user: {
-        isClient: user.isClient,
-        isMusician: user.isMusician,
-        firstLogin: user.firstLogin,
-        clerkId: user.clerkId,
-        userearnings: user?.earnings,
-        gigsBooked: user?.monthlyGigsBooked,
-        gigsPosted: user?.monthlyGigsPosted,
-        total: user?.totalSpent,
-        onboardingComplete: user?.onboardingComplete,
-      },
-      subscription: {
-        isPro:
-          user.tier === "pro" &&
-          (!user.nextBillingDate ||
-            new Date(user.nextBillingDate) > new Date()),
-        nextBillingDate: user?.nextBillingDate || null,
-        tier: user.tier,
-      },
-    };
-  } catch (error) {
-    console.error("Full error in getDashboardData:", error);
-    throw error;
-  }
-}
+//     return {
+//       user: {
+//         isClient: user.isClient,
+//         isMusician: user.isMusician,
+//         firstLogin: user.firstLogin,
+//         clerkId: user.clerkId,
+//         userearnings: user?.earnings,
+//         gigsBooked: user?.monthlyGigsBooked,
+//         gigsPosted: user?.monthlyGigsPosted,
+//         total: user?.totalSpent,
+//         onboardingComplete: user?.onboardingComplete,
+//         tier: user.tier,
+//       },
+//       subscription: {
+//         isPro:
+//           user.tier === "pro" &&
+//           (!user.nextBillingDate ||
+//             new Date(user.nextBillingDate) > new Date()),
+//         nextBillingDate: user?.nextBillingDate || null,
+//         tier: user.tier,
+//         lastBookingDate:user?.lastBookingDate
+//       },
+//     };
+//   } catch (error) {
+//     console.error("Full error in getDashboardData:", error);
+//     throw error;
+//   }
+// }
