68faffcd3671c81414fda4efb5f4943e22096834
zacharia muigai <zacheyshop@gmail.com>
2025-05-21 07:02:30 +0300

    implemented the booking and confirmation optmistic updates

---
+++ app/(exp)/layout.tsx
@@ -0,0 +1,28 @@
+import NotificationHandler from "@/components/NotificationHandler";
+import MobileSheet from "@/components/pages/MobileSheet";
+import { UserButton } from "@clerk/nextjs";
+
+import { Toaster } from "sonner";
+
+export default function ExperienceLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="bg-black h-full w-full overflow-scroll">
+      <div className="flex items-center justify-between mt-4 mx-6  ">
+        <MobileSheet />
+        <UserButton />
+      </div>
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 3000,
+        }}
+      />
+      <NotificationHandler />
+      {children}
+    </div>
+  );
+}
