47ed95cd66c5c33cdb30f82a26aedb7b31d98080
zacharia muigai <zacheyshop@gmail.com>
2025-07-08 02:06:40 +0300

    aDDED NEW FILTERINGG CAPABILITIES FOR SEARCHING MUSICIANS

--- app/api/cron/status/route.ts
+++ app/api/cron/status/route.ts
@@ -5,14 +5,14 @@ import cron from "node-cron";
 export const dynamic = "force-dynamic"; // Disable all caching
 export const revalidate = 0;
 // app/api/cron/status/route.ts
-export const config = {
-  api: {
-    bodyParser: false,
-    externalResolver: true,
-  },
-  // Completely bypass middleware
-  matcher: false,
-};
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//     externalResolver: true,
+//   },
+//   // Completely bypass middleware
+//   matcher: false,
+// };
 export async function GET() {
   try {
     // Create clean response object
